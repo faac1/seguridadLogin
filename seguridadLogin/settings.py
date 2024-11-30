5a9d59f6ce0af2bf022ddf509a8bb17b08c8361a
faac1 <fitofavo.2001@gmail.com>
2024-11-30 13:44:25 -0300

    Agregando dj-database-url

--- seguridadLogin/settings.py
+++ seguridadLogin/settings.py
@@ -138,7 +138,7 @@ LOGIN_URL = '/auth/login/'  # Página de login (si no está logueado)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
